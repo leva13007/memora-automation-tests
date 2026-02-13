45aa03ce7b7c7c145e128930d3357ad38ec035a2
Oleh Levchenko <leva13007@gmail.com>
2026-02-13 20:53:12 +0000

    (MAT-3) write TC_MC_0026_UI_H1_Desktop_DarkTheme_Test

--- src/test/java/dev/memora/ui/h1/TC_MC_0026_UI_H1_Desktop_DarkTheme_Test.java
+++ src/test/java/dev/memora/ui/h1/TC_MC_0026_UI_H1_Desktop_DarkTheme_Test.java
@@ -10,10 +10,10 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 import dev.memora.cssStyleAssertions.CssStyleAssertions;
 
-public class TC_MC_0025_UI_H1_Desktop_LightTheme_Test {
+public class TC_MC_0026_UI_H1_Desktop_DarkTheme_Test {
 
     String dataTestId = "test_h1";
-    String theme = "light";
+    String theme = "dark";
 
     String hostURL = "https://leva13007.github.io/memora-cards-storybook/";
     String iframe = "iframe.html?globals=";
@@ -22,7 +22,7 @@ public class TC_MC_0025_UI_H1_Desktop_LightTheme_Test {
     String args = "&args=data-testid:" + dataTestId;
     int width = 1280;
     int height = 480;
-    String textColor = "#0f172a";
+    String textColor = "#f5f7fb";
 
     String fontFamily = "Inter";
     String fontSize = "32px";
